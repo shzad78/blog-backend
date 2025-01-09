7930ade619e2b21f620864785d40467de0a94fe0
Shahzad Babar <babar@MacBook-Air-som-tillhor-Shahzad.local>
2025-01-09 18:11:04 +0100

    user-schema

--- models/user.js
+++ models/user.js
@@ -1,9 +1,15 @@
-const { Schema } = require("mongoose");
+
+const { Schema, model} = require("mongoose");
 
 const userSchema = new Schema({ 
     name: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true }
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    salt: { type: String, required: true },
+    profileImage: { type: String, default: "./public/images.defualt.jpeg" },
+    roles: { type: String, default: "user" ,Enum: ["user", "admin"]},
     
-});
-);
+}, {timestamp: true });
+
+const User = model("User", userSchema);
+module.exports = User;

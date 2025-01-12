ee7b07bb3c0921cf7ad9fc3397679ee39453d2ff
shzad78 <shzad78@example.com>
2025-01-12 22:56:12 +0100

    routes

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+const User = require("./models/user");
+const userRoutes = require("./routes/user"); // Import the user route
+
+mongoose.connect("mongodb://localhost:27017", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log("Connected to MongoDB");
+})
+.catch((err) => {
+  console.error("Failed to connect to MongoDB", err);
+});
 
 const app = express();
 const PORT = 3000;
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
+
+app.use("/user", userRoutes); // All routes in `user` will be prefixed with `/user`
+
 
 app.get("/", (req, res) => {
   res.render("home");
--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
-const { SECRET } = require("../config");
+
 
 const router = express.Router();
 
